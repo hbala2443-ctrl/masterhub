976b489d5176cc1c3fc34bf4a6c349a0d9308822
hbala2443-ctrl <hbala2443@gmail.com>
2026-08-04 16:55:55 +0530

    Update app.js

--- js/app.jsjs/app.js
+++ js/app.jsjs/app.js
@@ -1,1 +1,41 @@
-console.log("MasterHub Loaded Successfully");
+;const questions = [
+{
+id:1,
+exam:"CET",
+subject:"Rajasthan History",
+question:"कालीबंगा सभ्यता किस जिले में स्थित है?",
+options:[
+"हनुमानगढ़",
+"बीकानेर",
+"जोधपुर",
+"जयपुर"
+],
+answer:0
+},
+{
+id:2,
+exam:"CET",
+subject:"Rajasthan History",
+question:"आहड़ सभ्यता किस जिले में स्थित है?",
+options:[
+"उदयपुर",
+"कोटा",
+"अजमेर",
+"झालावाड़"
+],
+answer:0
+},
+{
+id:3,
+exam:"CET",
+subject:"Rajasthan History",
+question:"गणेश्वर सभ्यता किस जिले में स्थित है?",
+options:[
+"सीकर",
+"झुंझुनूं",
+"अलवर",
+"भरतपुर"
+],
+answer:0
+}
+];
