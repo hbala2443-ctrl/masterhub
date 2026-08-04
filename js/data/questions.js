047c2dedd373ce59b1fa2e84600573c805c0ca0c
hbala2443-ctrl <hbala2443@gmail.com>
2026-08-04 16:21:06 +0530

    Create questions.js

---
+++ js/data/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    exam: "CET",
+    subject: "History",
+    question: "भारत का संविधान कब लागू हुआ?",
+    options: [
+      "15 अगस्त 1947",
+      "26 जनवरी 1950",
+      "26 नवम्बर 1949",
+      "2 अक्टूबर 1950"
+    ],
+    answer: 1
+  },
+  {
+    id: 2,
+    exam: "CET",
+    subject: "History",
+    question: "भारत की राजधानी क्या है?",
+    options: [
+      "जयपुर",
+      "नई दिल्ली",
+      "मुंबई",
+      "भोपाल"
+    ],
+    answer: 1
+  }
+];
